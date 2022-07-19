4a689ac06da07ffb4408d7ed619274820ee5b1b8
curtybo <108523019+curtybo@users.noreply.github.com>
2022-07-19 15:07:49 +0200

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,9 @@
+(function (window) {
+    var helloSpeaker = {HelloSpeaker};
+    var speakWord = "Hello";
+  
+    helloSpeaker.speak = function Kevin, Anny, Paul, Peter, Ross, Pablo, {
+      console.log(Hello + "Hello" + Kevin, Anny, Paul, Peter, Ross, Pablo);
+}
+         windows.helloSpeaker = helloSpeaker;
+ })(window);
