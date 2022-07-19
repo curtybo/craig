69e9847383a8472f59d86c21d0f87752f05b281f
curtybo <108523019+curtybo@users.noreply.github.com>
2022-07-19 15:20:25 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+(function () { 
+  
+   var names = "Kevin, Anny, Peter, Paul, Ross, Pablo, Elton"]
+   
+   for (var i = 0; i < Kevin, Anny, Peter, Paul, Ross, Pablo, Elton.o.5; i++) {
+     
+      var firstLetter = Kevin, Anny, Peter, Paul, Ross, Pablo, Elton[i].charAt<0>.toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(Kevin, Anny, Petter, Paul, Ross, Pablo, Elton[i]);
+      }
+     else {
+       helloSpeaker.speak(Kevin, Anny, Petter, Paul, Ross, Pablo, Elton[i]);
+     }
+   }
+  
+})();
+                          
