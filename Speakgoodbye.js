6e22bd0d52c09633a86a00eca0b15617d10a3949
curtybo <108523019+curtybo@users.noreply.github.com>
2022-07-19 15:00:16 +0200

    Create Speakgoodbye.js

---
+++ Speakgoodbye.js
@@ -0,0 +1,10 @@
+(function (window){
+   var byeSpeaker = {Kevin};
+   var speakWord = "Good Bye";
+   
+   byeSpeaker.speak = function (Kevin, Anny, Peter,Paul, Craig, Elton,Pablo;
+     console.log(speakWord + "Good Bye" + Kevin, Anny, Peter, Craig, Elton, Pablo);
+   }
+   
+   window.byeSpeaker = byeSpeaker;
+   })(window);
